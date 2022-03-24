9bd61659027b144f62fe1c038eab7db574c0666c
DAVID FELIPE SANDOVAL GRANADOS <1057605874@bancopopular.com.co>
2022-03-24 09:38:11 -0500

    Primer commit para el repaso

---
+++ prueba.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(){
+
+	printf("hola mundo");
+	return 0;
+}
+
+typedef short int16;//reniimbre un tipo de dato para su uso posterior, el comilador hace la reasignacion, con el #define el preprocesador realiza las correspondientes declaraciones
+
+
+
---
+++ pruebaphp.php
@@ -0,0 +1,12 @@
+<?php
+
+$constante = 2;
+
+if ($constante!=2){
+
+	$constante++;
+}
+
+
+
+?>
